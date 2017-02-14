91d1f3218fb62ee1ef639ac26fa98f3df6bfc814
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-14 00:59:20 +0000

    prints body of a request for jquery repo on github

--- download_avatars.js
+++ download_avatars.js
@@ -2,11 +2,24 @@ var request = require('request');
 var GITHUB_USER = "zuzanatoldyova";
 var GITHUB_TOKEN = "1c8563b432b2c97cb0a77eefc49de460c62d33d7";
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var options = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+  request(options, cb);
+}
+
+function cb(error, response, body) {
+      if (error || response.statusCode != 200) {
+      console.log('Something wrong with the request');
+    } else {
+      console.log(body);
+    }
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
