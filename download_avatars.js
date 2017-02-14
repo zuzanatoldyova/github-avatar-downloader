1cffce80823f70368a049a0e7e05609a686fffa6
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-14 02:13:01 +0000

    changed to require command line arguments, returned message if some of them are missing

--- download_avatars.js
+++ download_avatars.js
@@ -13,33 +13,42 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+  if(!repoOwner || !repoName) {
+   console.log('The input is not valid, please enter repoOwner and repoName');
+   return;
+  }
+
   request(options, function(error, response, body) {
     if (error || response.statusCode != 200) {
       console.log('Something wrong with the request');
     } else {
       var contributors = JSON.parse(body);
       cb(contributors);
-      // console.log(body);
     }
   });
 }
 
 function cb(data) {
   for (var user of data) {
+    // console.log(user.avatar_url);
     var filePath = `./avatars/${user.login}.jpg`;
     downloadImageByURL(user.avatar_url, filePath);
   }
 }
 
-getRepoContributors(user_input[0], user_input[1], cb);
-
 function downloadImageByURL(url, filePath) {
   request.get(url)
+         .on('error', function(err) {
+            throw err;
+         })
          .pipe(fs.createWriteStream(filePath)
-            .on('finish', function(){
-              console.log('avatar downloaded');
-            })
+            // .on('finish', function(){
+            //   console.log('avatar downloaded');
+            // })
           );
 }
+
+getRepoContributors(user_input[0], user_input[1], cb);
+
 
 
