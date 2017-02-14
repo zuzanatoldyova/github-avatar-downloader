99cccd84d7800bb0649ac639e4f685bc2f8b9bb1
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-14 03:08:36 +0000

    input validation moved to download_avatars app

--- download_avatars.js
+++ download_avatars.js
@@ -2,9 +2,15 @@ var avatars = require('./avatars_api');
 var contributors = require('./contributors_parser');
 
 var userInput = process.argv.slice(2);
+var repoOwner = userInput[0];
+var repoName = userInput[1]
 
+if(!repoOwner || !repoName) {
+  console.log('The input is not valid, please enter repoOwner and repoName');
+  return;
+}
 
-contributors.getRepoContributors(userInput[0], userInput[1], avatars.extractAvatars);
+contributors.getRepoContributors(repoOwner, repoName, avatars.extractAvatars);
 
 
 
