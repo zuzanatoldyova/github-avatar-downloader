6f70e4eaa6086a297a92adbfc70afaf438dddf11
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-14 00:27:24 +0000

    added URL variable in the getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,16 @@
 var request = require('request');
+var GITHUB_USER = "zuzanatoldyova";
+var GITHUB_TOKEN = "1c8563b432b2c97cb0a77eefc49de460c62d33d7";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
